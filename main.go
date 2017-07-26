afb4eb3058c5a79272cd7c2e4880240be1fcf8e8
aftaberski <aftaberski@gmail.com>
2017-07-25 23:39:42 -0400

    Add a main fn to test out methods

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"./kademlia"
+)
+
+func main() {
+	node1 := kademlia.NewRandomNodeID()
+	node2 := kademlia.NewRandomNodeID()
+	fmt.Println("Node 1: ", node1)
+	fmt.Println("Node 2: ", node2)
+	fmt.Println("Equal? ", node1.Equals(node2))
+	fmt.Println("Less? ", node1.Less(node2))
+	fmt.Println("Xor", node1.Xor(node2))
+}
