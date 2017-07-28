18a08d9e8f619947ddf34ae669e4166222aa8763
aftaberski <aftaberski@gmail.com>
2017-07-28 17:50:37 -0400

    Add PrefixLen

--- main.go
+++ main.go
@@ -9,9 +9,12 @@ import (
 func main() {
 	node1 := kademlia.NewRandomNodeID()
 	node2 := kademlia.NewRandomNodeID()
-	fmt.Println("Node 1: ", node1)
-	fmt.Println("Node 2: ", node2)
-	fmt.Println("Equal? ", node1.Equals(node2))
-	fmt.Println("Less? ", node1.Less(node2))
+	// fmt.Println("Node 1: ", node1)
+	// fmt.Println("Node 2: ", node2)
+	// fmt.Println("Equal? ", node1.Equals(node2))
+	// fmt.Println("Less? ", node1.Less(node2))
 	fmt.Println("Xor", node1.Xor(node2))
+	fmt.Println("PrefixLen Xor:", node1.Xor(node2).PrefixLen())
+	// fmt.Println("PrefixLen Node 1:", node1.PrefixLen())
+	// fmt.Println("PrefixLen Node 2:", node2.PrefixLen())
 }
