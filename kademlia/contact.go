34442298b08a8ab5b07a31362ee9245fb39138ca
aftaberski <aftaberski@gmail.com>
2017-08-01 16:03:13 -0400

    Move contact/s to own file

---
+++ kademlia/contact.go
@@ -0,0 +1,35 @@
+package kademlia
+
+type Contact struct {
+	ID      NodeID
+	Address string
+}
+
+func NewContact(node NodeID, address string) Contact {
+	return Contact{
+		ID:      node,
+		Address: address,
+	}
+}
+
+type Contacts []Contact
+
+func (h Contacts) Less(i, j int) bool {
+	return h[i].ID.Less(h[j].ID)
+}
+
+func (h Contacts) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *Contacts) Push(x interface{}) {
+	*h = append(*h, x.(Contact))
+}
+
+func (contacts *Contacts) Pop() interface{} {
+	oldContacts := *contacts
+	oldLength := len(oldContacts)
+	element := oldContacts[oldLength-1]
+	*contacts = oldContacts[0 : oldLength-1]
+	return element
+}
