481c691623a380f3830b9056c9f4e021b92e206b
aftaberski <aftaberski@gmail.com>
2017-08-01 16:05:03 -0400

    Add node arg to PrefixLen

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	// fmt.Println("Equal? ", node1.Equals(node2))
 	// fmt.Println("Less? ", node1.Less(node2))
 	fmt.Println("Xor", node1.Xor(node2))
-	fmt.Println("PrefixLen Xor:", node1.Xor(node2).PrefixLen())
+	fmt.Println("PrefixLen Xor:", node1.Xor(node2).PrefixLen(node2))
 	// fmt.Println("PrefixLen Node 1:", node1.PrefixLen())
 	// fmt.Println("PrefixLen Node 2:", node2.PrefixLen())
 }
