82b661d9d6403bb9ab25e01bdd85ec516bad76bb
aftaberski <aftaberski@gmail.com>
2017-08-09 18:52:57 -0400

    Add note for future fixes

--- kademlia/find_node.go
+++ kademlia/find_node.go
@@ -1,6 +1,9 @@
 package kademlia
 
-import "container/heap"
+import (
+	"container/heap"
+	"sort"
+)
 
 type FindNodeRequest struct {
 	RPCHeader
@@ -55,9 +58,9 @@ func (kc *KademliaCore) FindNode(req FindNodeRequest, res *FindNodeResponse) err
 	return nil
 }
 
-func (k *Kademlia) IterativeFindNode(node NodeID, delta int, contacts chan Contacts) {
+func (k *Kademlia) IterativeFindNode(node NodeID, delta int, contacts chan Contacts) (ret Contacts) {
 	done := make(chan Contacts)
-	ret := make(Contacts, BucketSize)
+	ret = make(Contacts, BucketSize)
 	frontier := make(Contacts, BucketSize)
 
 	// So we don't accidentally add the same contact twice
@@ -70,4 +73,42 @@ func (k *Kademlia) IterativeFindNode(node NodeID, delta int, contacts chan Conta
 		seen[node.ID.String()] = true
 	}
 
+	// Start querying which puts responses on the done channel
+	// NOTE: Not accounting for unresponsive nodes! Right now, if a node never
+	// responds we'll wait here indefinitely
+	// TODO: Add a timeout for each RPC
+	pending := 0
+	for i := 0; pending < delta && frontier.Len() > 0; i++ {
+		pending++
+		contact := heap.Pop(&frontier).(Contact)
+		go k.sendQuery(contact, node, done)
+	}
+
+	// Iteratively look for closer nodes
+	for pending > 0 {
+		nodes := <-done
+		pending--
+		for _, n := range nodes {
+			// If we haven't seen this node before then add it
+			if _, ok := seen[n.ID.String()]; !ok {
+				ret = append(ret, n)
+				heap.Push(&frontier, n)
+				seen[n.ID.String()] = true
+			}
+		}
+
+		// And send the query
+		for i := 0; pending < delta && frontier.Len() > 0; i++ {
+			pending++
+			contact := heap.Pop(&frontier).(Contact)
+			go k.sendQuery(contact, node, done)
+		}
+
+		sort.Sort(ret)
+
+		if ret.Len() > BucketSize {
+			ret = ret[:BucketSize]
+		}
+	}
+	return
 }
